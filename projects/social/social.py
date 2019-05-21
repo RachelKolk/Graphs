f8ab03ca49d39b8efdb931c05371a18f884af6b1
RachelKolk <rach.m.obrien@gmail.com>
2019-05-21 17:24:42 -0400

    Degree of seperation part completed

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,19 @@
 import math
 import random
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue) 
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -53,6 +66,7 @@ class SocialGraph:
         # Add users
         # Time Complexity: O(n)
         # Space Complexity: O(n)
+        # creates users using the argument passed in
         for i in range(numUsers):
             self.addUser(f"User {i + 1}")
 
@@ -61,12 +75,17 @@ class SocialGraph:
         # totalFriendships = avgFriendships * numUsers
         # Time Complexity: O(n^2)
         # Space Complexity: O(n^2)
+        # create a place to hold the possible friendships
         possibleFriendships = []
+        # loops through the users list
         for userID in self.users:
+            # checks the firends options in the range of users - from the first to the last
             for friendID in range(userID + 1, self.lastID + 1):
+                # and then appends them to the possible friendship array
                 possibleFriendships.append((userID, friendID))
         # Time Complexity: O(n^2)
         # Space Complexity: O(1)
+        # shuffles the possible friendship array to make it random
         random.shuffle(possibleFriendships)
        
         # Time Complexity: O(n^2)
@@ -84,14 +103,40 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
+
         # !!!! IMPLEMENT ME
+        # bfs will return the shortest path
+
+        # create an empty queue
+        q = Queue()
+        # create an empty dictionary for visited friends
+        visited = {}  # Note that this is a dictionary, not a set
+        # add a path to the starting vertext to the queue
+        q.enqueue([userID])
+        # while the queue is not empty ...
+        while q.size() > 0:
+            # dequeue the first path
+            path = q.dequeue()
+            # assign v to the last vertex from the path
+            v = path[-1]
+            # if it has not been visited yet ...
+            if v not in visited:
+                # mark it as visited by adding it to the visited dict
+                visited[v] = path
+                # then enqueue paths to each of its neighbors to the queue
+                for next_vert in self.friendships[v]:
+                    path_copy = path.copy()
+                    path_copy.append(next_vert)
+                    q.enqueue(path_copy)
+        # return visited dictionary
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populateGraph(10, 2)
+    sg.populateGraph(500, 5)
     print(sg.friendships)
     connections = sg.getAllSocialPaths(1)
     print(connections)
+
+
